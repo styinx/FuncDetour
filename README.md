fbe9159fadad9e65eb469be474a994195f1865d9
Chris <zornchrisjo@gmail.com>
2024-03-19 17:55:57 +0100

    Update

--- README.md
+++ README.md
@@ -1,21 +1,11 @@
-# DLLInject
+# FuncDetour
 
-Redirect function calls via a detour.
-
-## Build
-
-```
-mkdir build
-cd build
-cmake ..
-```
+Simple function detour for x86 platforms (32 bit and 64 bit)
 
 ## How to use
 
-See also `/examples` folder.
-
 ```c++
-#include "Detour.hpp"
+#include "detour.hpp"
 
 int add(int a, int b)
 {
@@ -29,8 +19,14 @@ int hook_add()
 
 int main(int argc, char** argv)
 {
-    Detour::trampoline((Byte*)add, (Byte*)hook_add, 16);
-    add(5, 3); // 42
+    printf("Original add: %d\n", add(1, 2));  // 3
+
+    auto buffer = installHook(&add, &hook_add);
+    printf("Hooked add:   %d\n", add(1, 2));  // 42
+
+    uninstallHook(&add, buffer);
+    printf("Original add: %d\n", add(1, 2));  // 3
+
     return 0;
 }
 ```
--- examples/example.cpp
+++ examples/example.cpp
@@ -1,10 +1,8 @@
-#include "Detour.hpp"
+#include "detour.hpp"
 
-#include <iostream>
+#include <stdio.h>
 
-typedef int (*AddSignature)(int x, int y);
-AddSignature original_add;
-
+#pragma optimize("", off)
 int add(int a, int b)
 {
     return a + b;
@@ -14,24 +12,17 @@ int hook_add()
 {
     return 42;
 }
-
-int modify_add(int a, int b)
-{
-    return original_add(a, b) * 2;
-}
+#pragma optimize("", on)
 
 int main()
 {
-    // 'original_add' contains the contents of the original add(...) function.
-    // 'add' will jump to the 'hook_add' function.
-    original_add = (AddSignature)Detour::trampoline((Byte*)add, (Byte*)hook_add, 16);
+    printf("Original add: %d\n", add(1, 2));
 
-    // 8 as expected
-    std::cout << original_add(5, 3) << std::endl;
+    auto buffer = installHook(&add, &hook_add);
+    printf("Hooked add:   %d\n", add(1, 2));
 
-    // 42 from the hook_add
-    std::cout << add(5, 3) << std::endl;
+    uninstallHook(&add, buffer);
+    printf("Original add: %d\n", add(1, 2));
 
-    // 16 from the result of 'original_add' * 2
-    std::cout << modify_add(5, 3) << std::endl;
+    return 0;
 }
